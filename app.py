f4568a146c88a79805742ebd85210b0ab0aa8ec7
Tidings-SP <suryaprakashspro@gmail.com>
2024-04-10 11:26:47 +0530

    Update config

--- app.py
+++ app.py
@@ -1,34 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request 
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return 'Home Page Route'
 
 
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
-
-
-@app.route('/api')
-def api():
-    with open('data.json', mode='r') as my_file:
-        text = my_file.read()
-        return text
+@app.route('/<st>', methods = ['GET']) 
+def detect(st): 
+  
+    return jsonify({'data': st}) 
+  
 
 # if __name__ == "__main__":   
 #     app.run(debug=False)      

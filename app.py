01059db7ae780cf9799349800dc15826048429a7
Tidings-SP <suryaprakashspro@gmail.com>
2024-04-12 11:40:07 +0530

    Created Model

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, request 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
 
 
+clf = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__)
 
 
-
-
 @app.route('/<st>', methods = ['GET']) 
-def detect(st): 
-  
-    return jsonify({'data': st}) 
+def detect(st):
+    rs = 'Fake Promotion' if (clf.predict([st]) == 0) else 'Genuine Promotion'
+    return jsonify({'data': rs}) 
   
 
-# if __name__ == "__main__":   
-#     app.run(debug=False)      
+if __name__ == "__main__":   
+    app.run(debug=False)      

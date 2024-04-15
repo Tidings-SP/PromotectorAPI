d41d9b28a236cd559a3dc134ec6da95a980c7b1f
Tidings-SP <suryaprakashspro@gmail.com>
2024-04-15 18:25:31 +0530

    Update detect

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request 
+from flask_cors import CORS
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import moviepy.editor as mp
@@ -6,19 +7,19 @@ import speech_recognition as sr
 from deep_translator import GoogleTranslator
 import pickle
 
-
 clf = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__)
+CORS(app) 
 @app.route('/') 
 def index():
     return "<center><h1>To use the API put / followed by your text on the root url!</h1></center>"
-
 
 
 @app.route('/detect', methods = ['GET']) 
 def detect():
     url = request.args.get('url')
     is_eng_param = request.args.get('isEng')
+    print(is_eng_param)
     is_eng = is_eng_param.lower() in ['true', '1', 't', 'y', 'yes'] if is_eng_param else True
     data = extract_text_from_video(url, is_eng)
     rs = 'Fake Promotion' if (clf.predict([data]) == 0) else 'Genuine Promotion'
@@ -28,17 +29,17 @@ def detect():
   
 
 
-def extract_text_from_video(url, isEng = True):
+def extract_text_from_video(url, isEng):
     video = mp.VideoFileClip(url)
     audio_file = video.audio 
     audio_file.write_audiofile("2.wav")
     r = sr.Recognizer() 
-    trans = GoogleTranslator()
-    # print(type(audio_file))
+    # trans = GoogleTranslator()
+    # print(isEng)
     # Specify the language code for Tamil
-    r.lang = "ta-IN"
+    
 
-    with sr.AudioFile("/opt/render/project/src/2.wav") as source:
+    with sr.AudioFile("2.wav") as source:
         # Listen for the data (load audio to memory)
         audio_data = r.record(source)
     try:
@@ -46,11 +47,12 @@ def extract_text_from_video(url, isEng = True):
         if(isEng):
             text = r.recognize_google(audio_data) 
         else:
+            print("here")
+            r.lang = "ta-IN"
             text = r.recognize_google(audio_data, language='ta-IN') 
         print("You said: " + text)
     except sr.UnknownValueError:
         print("Could not understand audio")
-        print("You said: " + text)
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
